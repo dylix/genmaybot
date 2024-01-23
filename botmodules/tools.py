cd1b57e023c98498c8550a0aeae9ddef152fbd3c
Chris Hall <dylix98@gmail.com>
2024-01-23 07:44:46 -0700

    zwift cleanup

--- botmodules/tools.py
+++ botmodules/tools.py
@@ -95,7 +95,7 @@ def google_url(searchterm, regexstring):
 
 
 def load_html_from_url(url, readlength="", returnurl=False):
-    #url = fixurl(url)
+    url = fixurl(url)
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'}
     req = urllib.request.Request(url, None, headers=headers)
     pagetmp = urllib.request.urlopen(req)
