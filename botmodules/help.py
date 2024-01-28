70294cab21e4e2f673a588c9613f26ace0c9eeed
Chris Hall <dylix98@gmail.com>
2024-01-28 03:28:09 -0700

    !help cleanup

--- botmodules/help.py
+++ botmodules/help.py
@@ -4,15 +4,15 @@ def commands_help(bot, e):
     if command == "":
         help="The following bot commands are available:\n"
         for command in bot.bangcommands:
-            help+=command + "   "
+            help+=command + " "
         help+="\nUse !help <command> to get more detailed info on each command"
-        e.output = bot.tools['insert_newline'](help)
+        e.output = bot.tools['insert_at_closest_space'](help)
     elif command in bot.bangcommands and hasattr(bot.bangcommands[command],"helptext"):
         help = bot.bangcommands[command].helptext
 
         if hasattr(bot.bangcommands[command],"privateonly"):
             help+="\nNote: This command works only in a private message to the bot"
-        e.output = bot.tools['insert_newline'](help)
+        e.output = bot.tools['insert_at_closest_space'](help)
     else:
         e.output = "Incorrect command or no help available." 
 
